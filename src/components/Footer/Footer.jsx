28fd1144259e80cfb03427bfb9aee525527c7f35
Victor Brito <javibrito10@gmail.com>
2023-06-26 13:17:49 -0600

    repository link added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <footer>
-      This site was made by <b>Victor Brito</b>, see the repository <a>here</a>
+      This site was made by <b>Victor Brito</b>, see the repository <a href='https://github.com/VictorBrito13/section-dropdown-FM' target='_blank' rel='noreferrer noopener'>here</a>
     </footer>
   )
 }
